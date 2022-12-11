559389b4fc62f66ceb70f41f0f9ea1300dd9321f
Yasin <yasincoban1998@outlook.com>
2022-12-12 01:07:47 +0300

    html and css finished and axios get fake datas

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Post from '../Post/Post';
+import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import Post from "../Post/Post";
 import "./feed.css";
 
-
 export const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const res = await axios.get("/api/videos");
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPosts();
+  }, []);
+  console.log(posts);
   return (
-    <div className='feed'>
-        <Post/>
+    <div className="feed">
+      {posts.map((post) => (
+        <Post key={post.id}  post={post}/>
+      ))}
     </div>
-  )
-}
+  );
+};
--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./post.css";
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
-    <div className='post'>Post</div>
-  )
-}
+    <div className="post">
+      <div className="post-img-container">
+        <img
+          className="post-img"
+          src={post.img}
+          alt=""
+        />
+        <span className="post-duration">{post.duration}</span>
+        <div className="post-info-container">
+          <img
+            className="post-avatar"
+            src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+            alt=""
+          />
+          <div className="post-info">
+            <span className="post-title">React Form Validation</span>
+            <span className="post-channel">Yasin Çoban</span>
+            <span className="post-details">280K-1 week ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
