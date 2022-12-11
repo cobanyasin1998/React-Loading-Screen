5227158f28521b388d85836d2edfe0bde4c71515
Yasin <yasincoban1998@outlook.com>
2022-12-11 21:14:41 +0300

    client app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="container">
+      <>
+        <Sidebar />
+      </>
+    </div>
+  );
+}
+
+export default App;
